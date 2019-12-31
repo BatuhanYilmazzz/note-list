5740926261dc456fb5f68c22d6921c616426df46
Batu1905 <a.batuhan.yilmaz@gmail.com>
2019-12-31 22:19:18 +0300

    Updates

---
+++ precache-manifest.6dfe145a32ee528c1d269b843822ebee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e673bf7fec1d9e1a96e681fe08a0a6",
+    "url": "/NoteList/index.html"
+  },
+  {
+    "revision": "7fb82a8e86b1f3b7f85a",
+    "url": "/NoteList/static/css/main.2febd442.chunk.css"
+  },
+  {
+    "revision": "f6ce6512a508e11af13a",
+    "url": "/NoteList/static/js/2.45ba5dba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/NoteList/static/js/2.45ba5dba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fb82a8e86b1f3b7f85a",
+    "url": "/NoteList/static/js/main.701403cc.chunk.js"
+  },
+  {
+    "revision": "4ec9dc1d9d9e04615bd3",
+    "url": "/NoteList/static/js/runtime-main.5a2dc450.js"
+  }
+]);
